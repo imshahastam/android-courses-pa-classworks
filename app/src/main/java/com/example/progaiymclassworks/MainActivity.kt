86a8583bf37c9a791cef3d376996a088ac3b1980
imshahastam <shahzada.stamova@gmail.com>
2021-12-03 13:09:01 +0600

    second commit

---
+++ app/src/main/java/com/example/progaiymclassworks/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.progaiymclassworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
